a8eb1775b5805d858690d137b2a93d37e86906d2
Igor-SeVeR <isegorov@edu.hse.ru>
2021-04-13 17:37:40 +0300

    Added Batchers sort parallel algorithm

--- nonadaptive_sorts/nonadaptive_sorts.h
+++ nonadaptive_sorts/nonadaptive_sorts.h
@@ -7,6 +7,7 @@
 
 #include <vector>
 #include <iostream>
+#include <omp.h>
 
 // Class with all operations needed
 class operations {
@@ -107,6 +108,52 @@ public:
             operations::swap(arr[0], arr[size - i - 1]);
         }
     }
+};
+
+class batchers_parallel_sort {
+
+private:
+    template<typename T>
+    static void merge(std::vector<T> &arr, size_t l, size_t m, size_t r, size_t real_size) {
+        size_t buffer_size = r - l + 1;
+        omp_set_dynamic(0);
+        omp_set_num_threads(8);
+        for (size_t k = buffer_size / 2; k > 0; k /= 2)
+            for (size_t j = k % (buffer_size / 2); j + k < buffer_size; j += k + k) {
+                size_t i;
+                #pragma omp parallel for shared(arr, l, j, k, real_size) private(i)
+                for (i = 0; i < k; i++)
+                    if (l + j + i < real_size && l + j + i + k < real_size)
+                        operations::compexch(arr[l + j + i], arr[l + j + i + k]);
+            }
+    }
+
+public:
+    template<typename T>
+    static void merge_sort(std::vector<T> &arr, size_t l, size_t r, size_t real_size) {
+        if (r <= l)
+            return;
+        size_t m = (r + l) / 2;
+        merge_sort(arr, l, m, real_size);
+        merge_sort(arr, m + 1, r, real_size);
+        merge(arr, l, m, r, real_size);
+    }
+
+    template<typename T>
+    static void sort(std::vector<T> &arr) {
+        size_t closest_right_st = 1;
+        size_t size = arr.size();
+        size_t real_size = size;
+        if (size == 0)
+            return;
+        --size;
+        while (size > 0) {
+            size = size >> 1;
+            closest_right_st = closest_right_st << 1;
+        }
+        merge_sort(arr, 0, closest_right_st - 1, real_size);
+    }
+
 };
 
 

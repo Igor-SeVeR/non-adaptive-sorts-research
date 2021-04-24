217e389d469177a1debcd8de1ef33e14032affc6
Igor-SeVeR <isegorov@edu.hse.ru>
2021-04-24 09:44:48 +0300

    Added experiment, parallel batcher

--- experiment/experiment.cpp
+++ experiment/experiment.cpp
@@ -9,24 +9,80 @@ using namespace std;
 
 int main() {
     generator gen;
-    vector<int> arr = gen.generate_int(1000000, false, "");
-    vector<int> kek(arr.begin(), arr.end());
-    unsigned int start = clock();
-    qsort::sort(arr, 0, arr.size());
-    unsigned int end = clock();
-    for (int i = 0; i < arr.size() - 1; i++)
-        if (arr[i] > arr[i + 1]) {
-            std::cout << "Incorrect_sort";
-            return 0;
-        }
-    std::cout << "Correct_sort" << ' ' << end - start << '\n';
-    start = clock();
-    batchers_parallel_sort::sort(kek, 8);
-    end = clock();
-    for (int i = 0; i < kek.size() - 1; i++)
-        if (kek[i] > kek[i + 1]) {
-            std::cout << "Incorrect_sort";
-            return 0;
-        }
-    std::cout << "Correct_sort" << ' ' << end - start << '\n';
+    vector<int> arr = gen.generate_int(1000, false, "");
+    int n = 100;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        vector<int> kek(arr.begin(), arr.end());
+        unsigned int start = clock();
+        qsort::sort(kek, 0, arr.size());
+        unsigned int end = clock();
+        sum += end - start;
+    }
+    cout << "qsort = " << sum / n << '\n';
+
+    //testing merge
+    sum = 0;
+    for (int i = 0; i < n; i++) {
+        vector<int> kek(arr.begin(), arr.end());
+        unsigned int start = clock();
+        merge_sort::sort(kek);
+        unsigned int end = clock();
+        sum += end - start;
+    }
+    cout << "merge = " << sum / n << '\n';
+
+    //testing heap
+    sum = 0;
+    for (int i = 0; i < n; i++) {
+        vector<int> kek(arr.begin(), arr.end());
+        unsigned int start = clock();
+        heap_sort::sort(kek);
+        unsigned int end = clock();
+        sum += end - start;
+    }
+    cout << "heap = " << sum / n << '\n';
+
+    //testing bubble
+    sum = 0;
+    for (int i = 0; i < n; i++) {
+        vector<int> kek(arr.begin(), arr.end());
+        unsigned int start = clock();
+        bubble_sort::sort(arr);
+        unsigned int end = clock();
+        sum += end - start;
+    }
+    cout << "bubble = " << sum / n << '\n';
+
+    //testing non_par_batch
+    sum = 0;
+    for (int i = 0; i < n; i++) {
+        vector<int> kek(arr.begin(), arr.end());
+        unsigned int start = clock();
+        batchers_sort::sort(kek);
+        unsigned int end = clock();
+        sum += end - start;
+    }
+    cout << "non_par_batch = " << sum / n << '\n';
+    //testing par_batch
+    //sum = 0;
+    //for (int i = 0; i < n; i++) {
+    //    vector<int> kek(arr.begin(), arr.end());
+    //    unsigned int start = clock();
+    //    batchers_parallel_sort::sort(kek, 16);
+    //    unsigned int end = clock();
+    //    sum += end - start;
+    //}
+    //cout << "par_batch = " << sum / n << '\n';
+
+    //testing zig-zag
+    sum = 0;
+    for (int i = 0; i < n; i++) {
+        vector<int> kek(arr.begin(), arr.end());
+        unsigned int start = clock();
+        zig_zag_sort::sort(arr);
+        unsigned int end = clock();
+        sum += end - start;
+    }
+    cout << "zig_zag = " << sum / n << '\n';
 }
--- nonadaptive_sorts/nonadaptive_sorts.h
+++ nonadaptive_sorts/nonadaptive_sorts.h
@@ -8,6 +8,9 @@
 #include <vector>
 #include <iostream>
 #include <omp.h>
+#include <ctime>
+#include <random>
+#include <algorithm>
 
 // Class with all operations needed
 class operations {
@@ -116,18 +119,23 @@ class batchers_parallel_sort {
 private:
     template<typename T>
     static void merge_if_50000(std::vector<T> &arr, size_t l, size_t m, size_t r, size_t real_size, int number_of_threads) {
-        size_t buffer_size = r - l + 1;
         omp_set_dynamic(0);
-        omp_set_num_threads(8);
-        size_t i, k, j;
-        for (k = buffer_size / 2; k > 0; k /= 2) {
-        #pragma omp parallel
-        {
-                #pragma omp for private(j, i)
+        omp_set_num_threads(number_of_threads);
+        size_t buffer_size = r - l + 1;
+        size_t i, k = buffer_size / 2, j;
+        for (k; k > 100; k /= 2) {
+#pragma omp for private(i, j)
                 for (j = k % (buffer_size / 2); j < buffer_size - k; j += k + k) {
                     for (i = 0; i < k; i++)
                         if (l + j + i < real_size && l + j + i + k < real_size)
                             operations::compexch(arr[l + j + i], arr[l + j + i + k]);
+                }
+            }
+        for (k; k > 0; k /= 2) {
+            for (j = k % (buffer_size / 2); j < buffer_size - k; j += k + k) {
+                for (i = 0; i < k; i++) {
+                    if (l + j + i < real_size && l + j + i + k < real_size)
+                        operations::compexch(arr[l + j + i], arr[l + j + i + k]);
                 }
             }
         }
@@ -136,8 +144,6 @@ private:
     template<typename T>
     static void merge(std::vector<T> &arr, size_t l, size_t m, size_t r, size_t real_size, int number_of_threads) {
         size_t buffer_size = r - l + 1;
-        omp_set_dynamic(0);
-        omp_set_num_threads(number_of_threads);
         size_t i, k, j;
         for (k = buffer_size / 2; k > 0; k /= 2) {
             for (j = k % (buffer_size / 2); j < buffer_size - k; j += k + k) {
@@ -156,10 +162,10 @@ private:
         size_t m = (r + l) / 2;
         merge_sort(arr, l, m, real_size, number_of_threads);
         merge_sort(arr, m + 1, r, real_size, number_of_threads);
-        if (r - l + 1 >= 50000)
+        //if (r - l + 1 >= 10000)
             merge_if_50000(arr, l, m, r, real_size, number_of_threads);
-        else
-            merge(arr, l, m, r, real_size, number_of_threads);
+        //else
+        //    merge(arr, l, m, r, real_size, number_of_threads);
 
     }
 
@@ -167,6 +173,7 @@ public:
 
     template<typename T>
     static void sort(std::vector<T> &arr, int number_of_threads = 8) {
+
         size_t closest_right_st = 1;
         size_t size = arr.size();
         size_t real_size = size;
@@ -182,5 +189,119 @@ public:
 
 };
 
+class zig_zag_sort {
+
+private:
+
+    template<class RandomType>
+    static inline std::vector<int> gen_perm(int n, RandomType& rnd) {
+        std::vector<int> p(n);
+        std::iota(p.begin(), p.end(), 0);
+        std::shuffle(p.begin(), p.end(), rnd);
+        return p;
+    }
+
+    template<class RandomType>
+    static inline void epsHalver(int l1, int r1, int l2, int r2, int rev_eps, RandomType& rnd, std::vector<int> &arr, int real_size) {
+        int n = r1 - l1;
+        int r = std::max((int)std::ceil(2 * rev_eps * log(rev_eps)), 1);
+        for (int iter = 0; iter < r; ++iter) {
+            std::vector<int> p = gen_perm(n, rnd);
+            for (int i = 0; i < n; ++i) {
+                if (i + l1 < real_size && p[i] + l2 < real_size)
+                    operations::compexch(arr[i + l1], arr[p[i] + l2]);
+            }
+        }
+    }
+
+    static inline void bubbleSort(int l1, int r1, int l2, int r2, std::vector<int> &arr, int real_size) {
+        auto get_idx = [&](int i) {
+            if (i < r1 - l1) {
+                return l1 + i;
+            }
+            i -= r1 - l1;
+            return l2 + i;
+        };
+        int n = r1 - l1;
+        n <<= 1;
+        for (int i = 0; i < n - 1; ++i) {
+            for (int j = 0; j < n - 1 - i; ++j) {
+                if (get_idx(j + 1) < real_size)
+                    operations::compexch(arr[get_idx(j)], arr[get_idx(j + 1)]);
+            }
+        }
+    }
+
+    template<class RandomType>
+    static void Attenuate(int l1, int r1, int l2, int r2, int rev_eps, RandomType& rnd, std::vector<int> &arr, int real_size) {
+        int n = r1 - l1;
+        if (n <= 8) {
+            bubbleSort(l1, r1, l2, r2, arr, real_size);
+            return;
+        }
+        int m1 = (l1 + r1) >> 1, m2 = (l2 + r2) >> 1;
+
+        epsHalver(l1, m1, m1, r1, rev_eps, rnd, arr, real_size);
+        epsHalver(l2, m2, m2, r2, rev_eps, rnd, arr, real_size);
+        epsHalver(m1, r1, l2, m2, rev_eps, rnd, arr, real_size);
+        Attenuate(m1, r1, l2, m2, rev_eps, rnd, arr, real_size);
+
+        l1 = m1; r2 = m2;
+        m1 = (l1 + r1) >> 1, m2 = (l2 + r2) >> 1;
+
+        epsHalver(l1, m1, m1, r1, rev_eps, rnd, arr, real_size);
+        epsHalver(l2, m2, m2, r2, rev_eps, rnd, arr, real_size);
+        epsHalver(m1, r1, l2, m2, rev_eps, rnd, arr, real_size);
+        Attenuate(m1, r1, l2, m2, rev_eps, rnd, arr, real_size);
+    }
+
+    template<class RandomType>
+    static void Reduce(int l1, int r1, int l2, int r2, int rev_eps, RandomType& rnd, std::vector<int> &arr, int real_size) {
+        int n = r1 - l1;
+        if (n <= 8) {
+            bubbleSort(l1, r1, l2, r2, arr, real_size);
+            return;
+        }
+        epsHalver(l1, r1, l2, r2, rev_eps, rnd, arr, real_size);
+        Attenuate(l1, r1, l2, r2, rev_eps, rnd, arr, real_size);
+    }
+
+public:
+
+    static void sort(std::vector<int> &arr) {
+
+        size_t closest_right_st = 0;
+        size_t closest_right_st_val = 1;
+        size_t size = arr.size();
+        size_t real_size = size;
+        int eps = 15;
+        std::mt19937 rnd(time(nullptr));
+        --size;
+        while (size > 0) {
+            size = size >> 1;
+            closest_right_st = closest_right_st += 1;
+            closest_right_st_val <<= 1;
+        }
+        size = real_size;
+        for (int j = 1; j <= closest_right_st; ++j) {
+            size_t part_size = closest_right_st_val / (1 << (j));
+            for (int i = 1; i <= 1 << (j - 1); ++i) {
+                Reduce((i - 1) * (2 * part_size), (i - 1) * (2 * part_size) + part_size, i * 2 * part_size - part_size, i * 2 * part_size, eps, rnd, arr, size);
+            }
+            for (int i = 1; i <= (1 << (j)) - 1; i++) {
+                for (int k = 0; k < part_size; k++)
+                    if (i * part_size + k < size)
+                        operations::compexch(arr[(i - 1) * part_size + k], arr[i * part_size + k]);
+                Reduce((i - 1) * part_size, i * part_size, i * part_size, (i + 1) * part_size, eps, rnd, arr, size);
+            }
+            for (int i = (1 << j); i >= 2; i--) {
+                for (int k = 0; k < part_size; k++)
+                    if ((i - 1) * part_size + k < size)
+                        operations::compexch(arr[(i - 1) * part_size + k], arr[(i - 2) * part_size + k]);
+                Reduce((i - 2) * part_size, (i - 1) * part_size, (i - 1) * part_size, i * part_size, eps, rnd, arr, size);
+            }
+        }
+    }
+};
 
 #endif //COURSEWORK_NONADAPTIVE_SORTS_H
